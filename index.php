28988d5fab68ec48988553eb580e9e09d612889d
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-30 14:48:01 +0200

    update: fix bootstrap col, row and push finished project

--- index.php
+++ index.php
@@ -12,11 +12,14 @@
     <link rel="stylesheet" href="style.css">
 </head>
 
+<!-- meteo -->
+
 <body class="m-4">
     <header class="row">
         <div id="meteo" class="border col-lg-2 d-flex flex-wrap align-content-center justify-content-center col-sm-6 order-1 order-lg-1">
             <h3>METEO</h3>
         </div>
+
         <!-- titre edito -->
         <div id="titre" class="col-lg-8 text-center col-sm-12 order-lg-2">
             <a href="index.php" class="nav-link text-dark">
@@ -24,94 +27,100 @@
             </a>
             <h3 class="bg-danger col-3 offset-5" style="transform : rotate(-10deg);color:azure">L'info, la vraie</h3>
         </div>
+
         <!-- onglet contact -->
         <div id="connexion" class="border col-lg-2 order-sm-2 col-sm-6 order-2 order-lg-3">
             la redac: xxxxxxxxx@xxx.xx<br>
             Fax: xxxxxxxxxxxxxxx<br>
             Tel: xxxxxxxxxxxxxxx<br>
             <br>
-            <p>Service-abonné:xxxxxxxxxxxxxxxxxxx</p>
+            <p>Service-abonné:xxxxxxxxxx</p>
             <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenter">S'abonner</button>
             <button type="button" class="btn btn-danger"><img src="img/iconeShare.png" alt="icon share"></button>
         </div>
-        <div class="col-lg-11 text-right order-lg-4" id="date">
-            <?php
-            function quelleDate()
-            {
-                $date = date("d/m/Y");
-                return $date;
-            }
-            echo quelleDate(); ?>
+
+        <!-- date -->
+        <div class="col-lg-12 text-right order-lg-4" id="date">
+            <div class="date">
+                <?php
+                function quelleDate()
+                {
+                    $date = date("d/m/Y");
+                    return $date;
+                }
+                echo quelleDate(); ?>
+            </div>
+            <hr class="ml-5 mr-5 hidden-hr">
         </div>
+
+        <!-- navbar -->
+        <nav class="container-fluid navbar order-lg-5 navbar-expand-lg">
+            <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav nav-justified container-fluid">
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="international.php">International<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="#!">France<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="#!">Grand-Est<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="#!">Economie<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="#!">Culture<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link text-dark " href="#!">Le dossier de l'Edito<span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
 
-    <hr>
-    <nav class="container-fluid navbar order-lg-5 navbar-expand-lg">
-        <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <ul class="navbar-nav nav-justified container-fluid">
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="international.php">International<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="#!">France<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="#!">Grand-Est<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="#!">Economie<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="#!">Culture<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link text-dark " href="#!">Le dossier de l'Edito<span class="sr-only">(current)</span></a>
-                </li>
-            </ul>
+    <!-- group section -->
+    <section class="row mt-3">
+        <div class="col-lg-3 p-1 d-none d-lg-block">
+            <aside class="border col-lg-12 h-100" style="border-radius: 25px">
+                <h4 class="mt-n3"><span class="badge badge-secondary badge-danger">Group</span></h4>
+                <h3>Lorem ipsum...</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae molestias numquam odit aspernatur consequuntur
+                </p>
+                <img class="img-fluid" src="https://via.placeholder.com/250x250" alt="">
+                <h3>Lorem ipsum...</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae molestias numquam odit aspernatur consequuntur
+                </p>
+                <img class="img-fluid" src="https://via.placeholder.com/250x250" alt="">
+                <h3>Lorem ipsum...</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae molestias numquam odit aspernatur consequuntur
+                </p>
+            </aside>
         </div>
-    </nav>
 
-    <section class="row mt-4">
-        <aside class="border col-lg-2">
-            <h4 class="mt-n3"><span class="badge badge-secondary badge-danger">Group</span></h4>
-            <p>
-                <h2><b>Lorem ipsum ...</h2></b><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!
-            </p>
-            <img src="https://via.placeholder.com/150" class="img-fluid" alt="Responsive image">
-            <p>
-                <h2><b>Lorem ipsum ...</h2></b><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!
-            </p>
-            <img src="https://via.placeholder.com/150" class="img-fluid" alt="Responsive image">
-            <p>
-                <h2><b>Lorem ipsum ...</h2></b><br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!
-            </p>
-        </aside>
-
-
-        <article class="border col-lg-10">
-            <h4 class="mt-n3"><span class="badge badge-secondary badge-danger">International</span></h4>
-            <div class="d-flex w-100 h-50 ml-5 mt-5">
-                <div class="d-flex w-50">
-                    <img src="https://via.placeholder.com/600x300" class="img-fluid" alt="Responsive image">
+        <!-- international section -->
+        <div class="col-lg-9 p-1">
+            <article class="border col-lg-12 h-100" style="border-radius: 25px">
+                <h4 class="mt-n3"><span class="badge badge-secondary badge-danger">International</span></h4>
+                <img class="float-left m-3 img-fluid" src="https://via.placeholder.com/500x350" alt="">
+                <div class="mt-3">
+                    <h3>Lorem ipsumdolor</h3>
+                    <p class="clearfix text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit provident repudiandae neque quam ad architecto minus in qui porro deleniti! Dolores corrupti ad fuga itaque, ipsam mollitia culpa veniam consectetur id earum et adipisci. Possimus, aperiam molestiae, similique nulla debitis totam architecto cum qui consectetur et amet nemo quaerat minus aspernatur. Nostrum non recusandae officia impedit hic laudantium minima voluptatem, voluptates mollitia ad quibusdam enim ipsa alias. Ipsum, at. Eaque, magnam error facere dolor nemo fuga laborum alias asperiores natus iure quaerat mollitia voluptate labore veniam aliquam et? Dignissimos eius labore aut consectetur aliquid cumque vel aperiam fugit necessitatibus asperiores.<br><br>Lire la suite...</p>
+                    <div class="ml-3">
+                        <h3>Lorem ipsumdolor</h3>
+                        <p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit provident repudiandae neque quam ad architecto minus in qui porro deleniti! Dolores corrupti ad fuga itaque, ipsam mollitia culpa veniam consectetur id earum et adipisci. Possimus, aperiam molestiae, similique nulla debitis totam architecto cum qui consectetur et amet nemo quaerat minus aspernatur. Nostrum non recusandae officia impedit hic laudantium minima voluptatem, voluptates mollitia ad quibusdam enim ipsa alias. Ipsum, at. Eaque, magnam error facere dolor nemo fuga laborum alias asperiores natus iure quaerat mollitia voluptateipsam mollitia culpa veniam consectetur id earum et adipisci. Possimus, aperiam molestiae, similique nulla debitis totam architecto cum qui consectetur et amet nemo quaerat minus aspernatur. Nostrum non recusandae officia impedit hic laudantium minima voluptatem, voluptates mollitia ad quibusdam enim ipsa alias. Ipsum, at. Eaque, magnam error facere dolor n labore veniam aliquam et? Dignissimos eius labore aut consectetur aliquid cumque vel aperiam fugit necessitatibus asperiores.</p>
+                        <p class="text-center">Lire la suite...</p>
+                    </div>
                 </div>
-                <div class="align-items-start w-25 mx-auto text-justify">
-                    <p>
-                        <h2><b>Lorem ipsum dolor</h2></b><br>
-                    </p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!<br><br>Lire la suite ...</p>
-                </div>
-            </div>
-            <div class="d-flex flex-column text-justify ml-5 mt-5 mr-5">
-                <p><b>
-                        <h1>Lorem ipsum dolor</h1>
-                    </b></p>
-                <p>sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam! Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Loi, vero vel est fugit itaque quisquam!sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit itaque quisquam!Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolor culpa incidunt natus nihil, voluptatibus quasi, vero vel est fugit i<br><br></p>
-                <p class="text-center">Lire la suite ...</p>
-            </div>
-        </article>
+            </article>
+        </div>
     </section>
 
     <!-- Modal -->
@@ -146,7 +155,7 @@
                         <div class="input-group-prepend">
                             <div class="input-group-text"><img src="img/asterisque.png" alt=""></div>
                         </div>
-                        <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="*******">
+                        <input type="password" class="form-control" id="inlineFormInputGroup" placeholder="*******">
                     </div>
                 </div>
                 <div class="row">
@@ -197,25 +206,22 @@
         </div>
     </div>
 
-    <div class="d-flex justify-content-between align-items-center w-100">
-        <div class="text-center mt-4 w-100">
-            <div class="d-flex img-size">
-                <img src="https://www.kindpng.com/picc/b/102/1022212.png" class="img-fluid" alt="Responsive image" width="100">
+    <!-- footer & video -->
+    <footer class="row">
+        <div class="col-7 d-none d-lg-block">
+            <img src="https://www.kindpng.com/picc/b/102/1022212.png" alt="" width="100" class="float-left">
+            <h3 class="text-center mt-5 p-5">L'enquête de Yan peters</h3>
+            <div class="ml-5">
+                <h5 class="clearfix text-center text-justify margin-top-50%">Où est passé le Grisbi chez Total</h5><br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente necessitatibus, non unde dignissimos esse adipisci, sed nemo veniam molestias voluptatem nesciunt ut ipsa quaerat maiores itaque alias quibusdam minima! Aliquid quaerat nesciunt ex officia libero, sint reprehenderit, quae ullam quo ad, facilis voluptas. Earum harum accusamus provident in itaque delectus facilis cum distinctio nesciunt, fugiat culpa labore tempora? Molestias libero fugiat distinctio exercitationem minima quam expedita recusandae neque repellendus tempore harum dignissimos, eaque explicabo molestiae rerum amet provident commodi. Incidunt numquam totam ipsum hic, nobis doloribus mollitia blanditiis nesciunt, fugiat culpa labore tempora? Molestias libero fugiat distinctio exercitationem minima quam expedita recusandae neque repellendus tempore harum dignissimos, eaque explicabo molestiae.</p>
+                <p>Lire la suite...</p>
+                </a>
             </div>
-            <h1>L'enquête de Yan peters:</h1><br>
-            <p>
-                <h4>Ou est passé le grisbi chez total ?</h4>
-            </p>
-            <p class="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas expedita asperiores nemo quibusdam repellat dolorum odit hic. Eveniet, doloribus. Perferendis atque, tempora pariatur porro vitae vero quidem ut iure quam aspernatur esse repellat deserunt fuga accusantium minima rem debitis ex, quis ducimus quo perspiciatis? Aut dolore numquam accusantium iusto qui?m ipsum dolor sit amet consectetur adipisicing elit. Voluptas expedita asperiores nemo quibusdam repellat dolorum odit hic. Eveniet, doloribus. Perferendis atque, tempora pariatur porro vitae vero quidem ut iure quam aspernatur esse repellat deserunt fuga accusantium minima rem debitis ex, quis ducimus quo perspiciatis? Aut dolorm ipsum dolor sit amet consectetur adipisicing elit. Voluptas expedita asperiores nemo quibusdam repellat dolorum odit hic. Eveniet, doloribus. Perferendis atque, tempora pariatur porro vitae vero quidem ut iure quam aspernatur esse repellat deserunt fuga accusantium minima rem debitis ex, quis ducimus quo perspiciatis? Aut dolor
-            </p>
-            <p class="text-left">Lire la suite ...</p>
         </div>
-        <div class="ml-5 mr-n3 w-100 d-block">
-            <iframe src="https://www.youtube.com/embed/s5Bc2qHa-HY" height="400" width="100%" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <div class="col-lg-4 offset-lg-1 mt-lg-5 col-sm-12">
+            <iframe width="100%" height="400" src="https://www.youtube.com/embed/s5Bc2qHa-HY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
-    </div>
-    <footer>
     </footer>
 
     <!-- Optional JavaScript -->
